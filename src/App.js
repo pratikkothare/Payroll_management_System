c2b7053948fa0bb123bcf95937982274f8490e32
pratikkothare <kothareprateek@gmail.com>
2025-04-07 19:54:22 +0530

    initL COMMIT

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Main from './components/Main.js';
+import Sidebar from './components/Sidebar.js';
+function App() {
+  return (
+      <div className="wrapper">
+        <Main />
+        <Sidebar />
+      </div>
+    
+  );
+}
+
+export default App;
